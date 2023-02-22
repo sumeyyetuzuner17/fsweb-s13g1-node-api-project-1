730c513e2f89d1265e5490e46fa1725852ad9a69
sumeyyetuzuner17 <sumeyyedeniz34@gmail.com>
2023-02-22 17:32:28 +0300

    görevler yapıldı

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+
 const server = require('./api/server');
 
-const port = 9000;
+const port = 9002;
 
 // START YOUR SERVER HERE
+server.listen(port,()=>{
+    console.log("server is working at "+port);
+});
